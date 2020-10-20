8dd1c51466d5f20c08aed19fdfda992860cf03fc
Ray Hu <ray@isonohealth.com>
2020-10-19 21:25:05 -0700

    quest 5 serpent

--- Eliza.cpp
+++ Eliza.cpp
@@ -9,7 +9,8 @@
 #include <sstream>
 using namespace std;
 
-string toUpper(string &source) {
+string toUpper(string &source)
+{
   string outputStr(source);
   std::transform(outputStr.begin(), outputStr.end(), outputStr.begin(),
                  ::toupper);
@@ -17,27 +18,30 @@ string toUpper(string &source) {
 }
 
 string replaceStr(string &sourceStr, string toFind, string toReplace,
-                  bool caseSensitive) {
+                  bool caseSensitive)
+{
   std::string outputStr(sourceStr);
-  std::string lookupStr(sourceStr);
-  int compensation = toReplace.length() - toFind.length();
-  int compensationTimes = 0;
-  if (!caseSensitive) {
-    toFind = toUpper(toFind);
-    lookupStr = toUpper(lookupStr);
-  }
-  size_t found = lookupStr.find(toFind);
-  while (found != std::string::npos) {
-    // cout << "Found " << toFind << " at " << found << " of " <<  lookupStr <<
-    // "\n";
-    found = found + compensation * compensationTimes;
+  // std::string lookupStr(sourceStr);
+  // int compensation = toReplace.length() - toFind.length();
+  // int compensationTimes = 0;
+  // if (!caseSensitive)
+  // {
+  //   toFind = toUpper(toFind);
+  //   lookupStr = toUpper(lookupStr);
+  // }
+  size_t found = outputStr.find(toFind);
+  while (found != std::string::npos)
+  {
+    // cout << "Found " << toFind << " at " << found << " of " <<  lookupStr << "\n";
+    // found = found + compensation * compensationTimes;
     // cout << "To replace " << toFind << " from " << found << " to " << found +
     // toFind.size() << " to "<< toReplace << " of "<< outputStr << "\n"; cout
     // << "Before replace: " << outputStr << "\n";
+    // outputStr.resize(outputStr.size() + compensation);
     outputStr.replace(found, toFind.size(), toReplace);
-    // cout << "After replace: " << outputStr << "\n";
-    compensationTimes++;
-    found = lookupStr.find(toFind, found + toReplace.length());
+    // compensationTimes++;
+    // if (compensation==0){
+    found = outputStr.find(toFind, found + toFind.size());
   }
   return outputStr;
 }
@@ -49,80 +53,85 @@ string replaceStr(string &sourceStr, string toFind, string toReplace,
 // rely on the result being returned.
 // TODO - Your code for rotate_vowels goes here
 
-string rotate_vowels(string &s) {
+string rotate_vowels(string &s)
+{
   std::string rotate(s);
-  rotate = replaceStr(rotate, "u", "##", true);
+  rotate = replaceStr(rotate, "u", "#", true);
   rotate = replaceStr(rotate, "o", "u", true);
   rotate = replaceStr(rotate, "i", "o", true);
   rotate = replaceStr(rotate, "e", "i", true);
   rotate = replaceStr(rotate, "a", "e", true);
-  rotate = replaceStr(rotate, "##", "a", true);
+  rotate = replaceStr(rotate, "#", "a", true);
   return rotate;
 }
 
 // Return a string in which all occurrences of s have been replaced by th
 // TODO - Your code for lispify goes here
-string lispify(string s) {
+string lispify(string s)
+{
   std::string s2th(s);
   return replaceStr(s2th, "s", "th", false);
 }
-string lispify2(string s) {
+string lispify2(string s)
+{
   std::string s2th(s);
   s2th = replaceStr(s2th, "s", "th", true);
   return replaceStr(s2th, "S", "Th", true);
 }
 // Enter the user-interaction loop as described earlier
-void enter() {
+void enter()
+{
   // TODO - Your code here
-  string inputStr;
-  do {
-    try {
-      cout << "What?\n";
+  string inputStr("dsajsjj sdasd asd as da sd asd as das da sd s dsa das da sd sad");
+  cout << "What?" << endl;
+  do
+  {
+    try
+    {
       std::getline(std::cin, inputStr);
-      cout<<inputStr.length()<<"n";
-      if (inputStr.length() == 0) {
+      if (inputStr.length() == 0)
+      {
+        cout << "What?" << endl;
         continue;
       }
       cout << "    " << inputStr << "\n\n";
-      if (inputStr.find("!") != std::string::npos) {
+      if (inputStr.find("!") != std::string::npos)
+      {
         cout << "OMG! You don't say!! " << inputStr << "!!!!!\n";
         continue;
       }
       if ((inputStr.find("what") != std::string::npos) ||
-          (inputStr.find("why") != std::string::npos)) {
+          (inputStr.find("why") != std::string::npos))
+      {
         cout << "I'm sorry, I don't like questions that contain what or why.\n";
         continue;
       }
-      if (inputStr.find("s") != std::string::npos) {
-        cout << "Interethting.When did you thtop thtopping your\n";
-        cout << "thibilanth ? \n";
+      if (inputStr.find('s') != std::string::npos)
+      {
+        cout << "Interethting. When did you thtop thtopping your thibilanth? \n";
         cout << lispify(inputStr) << "! Sheesh! Now what?\n";
         continue;
       }
       if ((inputStr == "bye") || (inputStr == "Bye") || (inputStr == "quit") ||
-          (inputStr == "Quit")) {
+          (inputStr == "Quit"))
+      {
         cout << "Ok Bye. Nice being a force of change in your life.\n";
-        //break;
+        break;
       }
       int remainder = rand() % 10;
-      if (remainder != 8 && remainder != 9) {
-        cout << rotate_vowels(inputStr) << "\n";
-      } else {
+      if (remainder != 8 && remainder != 9)
+      {
+        cout << rotate_vowels(inputStr) << "?\n";
+      }
+      else
+      {
         cout << "Huh? Why do you say: " << inputStr << "?\n";
       }
-    } catch (int e) {
+    }
+    catch (int e)
+    {
       cout << "An exception occurred. Exception Nr. " << e << '\n';
     }
 
   } while (true);
-}
-
-int main(int argc, char const *argv[]) {
-  cout << lispify("sixty Six") << "\n";
-  cout << lispify2("sixty Six") << "\n";
-
-  string s = "that's really cool";
-  cout << rotate_vowels(s) << "\n";
-  enter();
-  return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
-// cs2a_quests.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "Eliza.h"
+using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    std::cout << lispify("sixty six");
+    cout << lispify("the handsome man ran with no handsome mat") << "\n";
+    cout << lispify2("the hungry rainbow kissed with the yummy wise gal") << "\n";
+    cout << lispify2("no hot wise guy kissed in no handsome cat") << "\n";
+
+    string s = "that's really cool";
+    cout << rotate_vowels(s) << "\n";
+    enter();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
